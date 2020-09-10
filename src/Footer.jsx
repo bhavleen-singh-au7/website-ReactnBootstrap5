f5c1b1e8df2438d3a2f84dd0fa9bf4d9a9ac5b33
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-09-10 22:41:09 +0530

    Footer Added

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="w-100 bg-ligh text-center">
+        <p>&copy; 2020 Singh Electronics. All Right Reserved.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
