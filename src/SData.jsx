4ecb80b1ee31869037471d662f684bfdc9a6833f
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-09-10 21:24:13 +0530

    2 more components added

---
+++ src/SData.jsx
@@ -0,0 +1,35 @@
+import web from "./images/s1.jpg";
+import app from "./images/s2.jpg";
+import software from "./images/s3.jpg";
+import digital from "./images/s4.jpg";
+import android from "./images/s5.jpg";
+import marketing from "./images/s6.jpg";
+
+const SData = [
+  {
+    imgsrc: web,
+    title: "Web Development",
+  },
+  {
+    imgsrc: app,
+    title: "App Development",
+  },
+  {
+    imgsrc: software,
+    title: "Software Development",
+  },
+  {
+    imgsrc: digital,
+    title: "Digital Marketing",
+  },
+  {
+    imgsrc: android,
+    title: "Android Development",
+  },
+  {
+    imgsrc: marketing,
+    title: "Marketing",
+  },
+];
+
+export default SData;
